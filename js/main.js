6bc599b4e70300f997004a799783202d6c6a44bf
Sean McPherson <seanmcp25@gmail.com>
2017-08-02 10:37:43 -0400

    Major functions and design

--- js/main.js
+++ js/main.js
@@ -8,7 +8,14 @@ let groupRemainder = document.getElementById("groupRemainder");
 let countOff = document.getElementById("countOff");
 let resetGroups = document.getElementById("resetGroups");
 
+function resetOutput(){
+  finalGroups.textContent = "";
+  groupRemainder.textContent = "";
+  countOff.textContent = "";
+};
+
 calcGroups.addEventListener('click', function(){
+  resetOutput();
   let peopleVal = numPeople.value;
   let groupsVal = numGroups.value;
   let perGroupVal = peoplePerGroup.value;
@@ -25,23 +32,27 @@ calcGroups.addEventListener('click', function(){
     let calcRem = peopleVal % groupsVal;
 
     finalGroups.textContent = `There will be ${groupsVal - calcRem} group(s) of ${calcDown}`;
-    groupRemainder.textContent = `with ${calcRem} group(s) of ${calcDown + 1}`;
+    if (calcRem > 0) {
+      groupRemainder.textContent = `with ${calcRem} group(s) of ${calcDown + 1}`;
+    }
     countOff.textContent = `Count off by ${calcDown}s`
 
   } else if (perGroupVal) {
     let calcDown = Math.floor(peopleVal / perGroupVal);
     let calcRem = peopleVal % perGroupVal;
+    console.log("perGroupVal: ", perGroupVal);
+    console.log("Plus one: ", perGroupVal + 1);
 
-    finalGroups.textContent = `There will be ${calcDown - calcRem} group(s) of ${perGroupVal}`;
-    groupRemainder.textContent = `with ${calcRem} group(s) of ${perGroupVal + 1}`;
+    finalGroups.textContent = `There will be ${calcDown} group(s) of ${perGroupVal}`;
+    if (calcRem > 0) {
+      groupRemainder.textContent = `with one group of ${calcRem}`;
+    }
     countOff.textContent = `Count off by ${perGroupVal}s`;
   }
 });
 resetGroups.addEventListener('click', function() {
-  numPeople.innerHTML = "";
-  numGroups.innerHTML = "";
-  peoplePerGroup.innerHTML = "";
-  finalGroups.textContent = "";
-  groupRemainder.textContent = "";
-  countOff.textContent = "";
+  numPeople.value = "";
+  numGroups.value = "";
+  peoplePerGroup.value = "";
+  resetOutput();
 })

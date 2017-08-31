427532c1cd5a3c80e38ec148982313c47d783c9a
Sean McPherson <seanmcp25@gmail.com>
2017-08-31 09:10:59 -0400

    Change js variables from html id

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 // Initialize global variables
-let numPeople = document.getElementById("numPeople");
-let numGroups = document.getElementById("numGroups");
-let peoplePerGroup = document.getElementById("peoplePerGroup");
-let calcGroups = document.getElementById("calcGroups");
-let finalGroups = document.getElementById("finalGroups");
-let groupRemainder = document.getElementById("groupRemainder");
-let countOff = document.getElementById("countOff");
-let resetGroups = document.getElementById("resetGroups");
+let numberOfPeople = document.getElementById("numPeople");
+let numberOfGroups = document.getElementById("numGroups");
+let peoplePerGroup = document.getElementById("pplPerGroup");
+let calculateGroups = document.getElementById("calcGroups");
+let resultGroups = document.getElementById("finalGroups");
+let anyRemainder = document.getElementById("groupRemainder");
+let countOffBy = document.getElementById("countOff");
+let resetButton = document.getElementById("resetGroups");
 
 function resetOutput(){
-  finalGroups.textContent = "";
-  groupRemainder.textContent = "";
-  countOff.textContent = "";
+  resultGroups.textContent = "";
+  anyRemainder.textContent = "";
+  countOffBy.textContent = "";
 };
 
-calcGroups.addEventListener('click', function(){
+calculateGroups.addEventListener('click', function(){
   resetOutput();
-  let peopleVal = numPeople.value;
-  let groupsVal = numGroups.value;
+  let peopleVal = numberOfPeople.value;
+  let groupsVal = numberOfGroups.value;
   let perGroupVal = peoplePerGroup.value;
 
   if (!peopleVal) {
@@ -30,26 +30,26 @@ calcGroups.addEventListener('click', function(){
     let calcDown = Math.floor(peopleVal / groupsVal);
     let calcRem = peopleVal % groupsVal;
 
-    finalGroups.textContent = `There will be ${groupsVal - calcRem} group(s) of ${calcDown}`;
+    resultGroups.textContent = `There will be ${groupsVal - calcRem} group(s) of ${calcDown}`;
     if (calcRem > 0) {
-      groupRemainder.textContent = `with ${calcRem} group(s) of ${calcDown + 1}`;
+      anyRemainder.textContent = `with ${calcRem} group(s) of ${calcDown + 1}`;
     }
-    countOff.textContent = `Count off by ${calcDown}s`
+    countOffBy.textContent = `Count off by ${calcDown}s`
 
   } else if (perGroupVal) {
     let calcDown = Math.floor(peopleVal / perGroupVal);
     let calcRem = peopleVal % perGroupVal;
 
-    finalGroups.textContent = `There will be ${calcDown} group(s) of ${perGroupVal}`;
+    resultGroups.textContent = `There will be ${calcDown} group(s) of ${perGroupVal}`;
     if (calcRem > 0) {
-      groupRemainder.textContent = `with one group of ${calcRem}`;
+      anyRemainder.textContent = `with one group of ${calcRem}`;
     }
-    countOff.textContent = `Count off by ${perGroupVal}s`;
+    countOffBy.textContent = `Count off by ${perGroupVal}s`;
   }
 });
-resetGroups.addEventListener('click', function() {
-  numPeople.value = "";
-  numGroups.value = "";
+resetButton.addEventListener('click', function() {
+  numberOfPeople.value = "";
+  numberOfGroups.value = "";
   peoplePerGroup.value = "";
   resetOutput();
 })
